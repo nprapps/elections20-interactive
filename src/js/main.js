6edb871d86a666649cc8ec03b412581baf3b9a5d
thomaswilburn <twilburn@npr.org>
2020-08-10 09:33:02 -0500

    Centralize JS/LESS bundles

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 // require("./lib/pym");
+
+import $ from "./lib/qsa";
+import { h, render } from 'preact';
+
+import { GetCaughtUp } from "./includes/get-caught-up.js";
+import { BigBoardCore } from './includes/big-board-core.js';
+
+$(".big-board").forEach(function(board) {
+  var json = board.dataset.source;
+  var title = board.dataset.title;
+
+  render(<BigBoardCore json="{json}" title="{title}"/>, board);
+});
+
+$(".get-caught-up-wrapper").forEach(function(gcu) {
+  render(<GetCaughtUp />, gcu);
+});
