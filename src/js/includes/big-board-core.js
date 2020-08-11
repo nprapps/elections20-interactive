96d01ca7119f352ccf34767893b5d400d0e3ae12
ruthwtalbot <59851648+ruthwtalbot@users.noreply.github.com>
2020-08-11 12:15:18 -0400

    Run prettier locally

--- src/js/includes/big-board-core.js
+++ src/js/includes/big-board-core.js
@@ -1,7 +1,7 @@
 // import { h, createProjector } from 'maquette';
-import { h, Component, Fragment } from "preact";
-import { buildDataURL, getHighestPymEmbed } from "./helpers.js";
-import gopher from "../gopher.js";
+import { h, Component, Fragment } from 'preact';
+import { buildDataURL, getHighestPymEmbed } from './helpers.js';
+import gopher from '../gopher.js';
 
 var copyBop = window.copy.bop;
 
@@ -60,15 +60,15 @@ export class BigBoardCore extends Component {
         </div>
         <div class="results">
           <div class="column first">
-            {results.firstCol.map((res) => this.renderResultsTable(res))}
+            {results.firstCol.map(res => this.renderResultsTable(res))}
           </div>
           <div class="column second">
-            {results.secondCol.map((res) => this.renderResultsTable(res))}
+            {results.secondCol.map(res => this.renderResultsTable(res))}
           </div>
         </div>
         <div class="footer">
           <p>
-            {" "}
+            {' '}
             Source: AP <span>as of lastupdated ET</span>
           </p>
         </div>
@@ -84,10 +84,10 @@ export class BigBoardCore extends Component {
         <Fragment>
           <h2 class="bucketed-group">
             <span>
-              {isBucketedByTime(column["category"])
-                ? column["category"] + " ET"
-                : column["category"]}
-              {column.overflow ? " (continued) " : ""}
+              {isBucketedByTime(column['category'])
+                ? column['category'] + ' ET'
+                : column['category']}
+              {column.overflow ? ' (continued) ' : ''}
             </span>
           </h2>
           <table class="races">
@@ -117,7 +117,7 @@ export class BigBoardCore extends Component {
                 </th>
               </tr>
             </thead>
-            {races.map((race) => this.renderRace(race))}
+            {races.map(race => this.renderRace(race))}
           </table>
         </Fragment>
       );
@@ -127,7 +127,6 @@ export class BigBoardCore extends Component {
   }
 
   renderRace(race) {
-
     var result1 = race['results']['result1'];
     var result2 = race['results']['result2'];
     var winningResult = race['results']['winningResult'];
@@ -139,58 +138,58 @@ export class BigBoardCore extends Component {
     var firstCand = (
       <Fragment>
         <td class="pickup"></td>
-        <th class="state">{race["label"]}</th>
-        <td class={"candidate " + firstCandClasses}>
+        <th class="state">{race['label']}</th>
+        <td class={'candidate ' + firstCandClasses}>
           <span class="fname">
-            {" "}
-            {result1["first"] ? result1["first"] + " " : ""}
+            {' '}
+            {result1['first'] ? result1['first'] + ' ' : ''}
           </span>
           <span class="lname">
-            {results["uncontested"]
-              ? result1["last"] + " (Uncontested)"
-              : result1["last"]}
+            {results['uncontested']
+              ? result1['last'] + ' (Uncontested)'
+              : result1['last']}
           </span>
         </td>
-        <td class={"candidate-total " + firstCandClasses}>
-          {results["uncontested"] ? (
-            ""
+        <td class={'candidate-total ' + firstCandClasses}>
+          {results['uncontested'] ? (
+            ''
           ) : (
             <span class="candidate-total-wrapper">
-              {Math.round(result1["votepct"] * 100)}
+              {Math.round(result1['votepct'] * 100)}
             </span>
           )}
         </td>
       </Fragment>
     );
 
-    var secondCandClasses = results["uncontested"]
-      ? ""
+    var secondCandClasses = results['uncontested']
+      ? ''
       : createClassesForBoardCells(result2, coloredParties);
-    var secondCand = results["uncontested"] ? (
+    var secondCand = results['uncontested'] ? (
       <div></div>
     ) : (
       <Fragment>
         <td class="candidate-total-spacer"></td>
-        <td class={"candidate-total " + secondCandClasses}>
-          {results["uncontested"] ? (
-            ""
+        <td class={'candidate-total ' + secondCandClasses}>
+          {results['uncontested'] ? (
+            ''
           ) : (
             <span class="candidate-total-wrapper">
-              {Math.round(result2["votepct"] * 100)}
+              {Math.round(result2['votepct'] * 100)}
             </span>
           )}
         </td>
-        <td class={"candidate " + secondCandClasses}>
+        <td class={'candidate ' + secondCandClasses}>
           <span class="fname">
-            {" "}
-            {result2["first"] ? result2["first"] + " " : ""}
+            {' '}
+            {result2['first'] ? result2['first'] + ' ' : ''}
           </span>
-          <span class="lname">{result2["last"]}</span>
+          <span class="lname">{result2['last']}</span>
         </td>
         <td class="results-status">
-          {!results["uncontested"]
-            ? this.calculatePrecinctsReporting(result1["precinctsreportingpct"])
-            : ""}
+          {!results['uncontested']
+            ? this.calculatePrecinctsReporting(result1['precinctsreportingpct'])
+            : ''}
         </td>
       </Fragment>
     );
@@ -207,7 +206,7 @@ export class BigBoardCore extends Component {
     if (this.state.title.match(/^House/)) {
       var bop = this.state.house_bop;
       return this.renderCongressBOP(bop);
-    } else if (this.state.title.indexOf("Senate") !== -1) {
+    } else if (this.state.title.indexOf('Senate') !== -1) {
       var bop = this.state.senate_bop;
       return this.renderCongressBOP(bop);
     }
@@ -222,49 +221,49 @@ export class BigBoardCore extends Component {
 
   renderCongressBOP(bop, chamber) {
     // TODO: refactor this to make cleaner
-    const demSeats = bop["Dem"]["seats"];
-    const gopSeats = bop["GOP"]["seats"];
-    const indSeats = bop["Other"]["seats"];
+    const demSeats = bop['Dem']['seats'];
+    const gopSeats = bop['GOP']['seats'];
+    const indSeats = bop['Other']['seats'];
 
     var netGain = 0;
-    var netGainParty = "no-change";
-    var netGainPartyLabel = "No change";
-    var netGainTitle = "";
-    var netGainExplanation = copyBop["pickups_" + chamber];
+    var netGainParty = 'no-change';
+    var netGainPartyLabel = 'No change';
+    var netGainTitle = '';
+    var netGainExplanation = copyBop['pickups_' + chamber];
 
-    if (bop["Dem"]["pickups"] > 0) {
-      netGain = bop["Dem"]["pickups"];
-      netGainParty = "dem";
-      netGainPartyLabel = "Dem.";
-      netGainTitle = copyBop["pickups_" + netGainParty];
-      netGainTitle = netGainTitle.replace("___PICKUPS___", netGain);
-    } else if (bop["GOP"]["pickups"] > 0) {
-      netGain = bop["GOP"]["pickups"];
-      netGainParty = "gop";
-      netGainPartyLabel = "GOP";
-      netGainTitle = copyBop["pickups_" + netGainParty];
-      netGainTitle = netGainTitle.replace("___PICKUPS___", netGain);
+    if (bop['Dem']['pickups'] > 0) {
+      netGain = bop['Dem']['pickups'];
+      netGainParty = 'dem';
+      netGainPartyLabel = 'Dem.';
+      netGainTitle = copyBop['pickups_' + netGainParty];
+      netGainTitle = netGainTitle.replace('___PICKUPS___', netGain);
+    } else if (bop['GOP']['pickups'] > 0) {
+      netGain = bop['GOP']['pickups'];
+      netGainParty = 'gop';
+      netGainPartyLabel = 'GOP';
+      netGainTitle = copyBop['pickups_' + netGainParty];
+      netGainTitle = netGainTitle.replace('___PICKUPS___', netGain);
     }
 
-    const chamberWinner = bop["npr_winner"];
-    const uncalledRaces = bop["uncalled_races"];
+    const chamberWinner = bop['npr_winner'];
+    const uncalledRaces = bop['uncalled_races'];
 
     return (
       <div class="leaderboard">
         <div class="results-header-group net-gain">
-          <h2 class={"party " + netGainParty} title={netGainTitle}>
-            <label>{copyBop["pickups_gain"]}</label>
+          <h2 class={'party ' + netGainParty} title={netGainTitle}>
+            <label>{copyBop['pickups_gain']}</label>
             <abbr title={netGainTitle}>
-              {netGain > 0 ? netGainPartyLabel + "+" + netGain : netGain}
+              {netGain > 0 ? netGainPartyLabel + '+' + netGain : netGain}
             </abbr>
           </h2>
         </div>
-        {this.getTopLevelHeaderGroup("Dem.", "Dem", demSeats)}
-        {this.getTopLevelHeaderGroup("GOP.", "GOP", gopSeats)}
-        {this.getTopLevelHeaderGroup("Ind.", "Other", indSeats)}
+        {this.getTopLevelHeaderGroup('Dem.', 'Dem', demSeats)}
+        {this.getTopLevelHeaderGroup('GOP.', 'GOP', gopSeats)}
+        {this.getTopLevelHeaderGroup('Ind.', 'Other', indSeats)}
         {this.getTopLevelHeaderGroup(
-          "Not Yet Called",
-          "Not-Called",
+          'Not Yet Called',
+          'Not-Called',
           uncalledRaces
         )}
       </div>
@@ -273,10 +272,10 @@ export class BigBoardCore extends Component {
 
   getTopLevelHeaderGroup(label, party, data, winner) {
     return (
-      <div class={"results-header-group " + party.toLowerCase()}>
+      <div class={'results-header-group ' + party.toLowerCase()}>
         <h2 class="party">
           <label>
-            {winner === party ? <i class="icon.icon-ok"></i> : ""}
+            {winner === party ? <i class="icon.icon-ok"></i> : ''}
             {label}
           </label>
           <abbr>{data}</abbr>
@@ -335,7 +334,7 @@ export class BigBoardCore extends Component {
         }
         return {
           firstCol: sortedCategories.slice(0, i + 1),
-          secondCol: sortedCategories.slice(i + 1, sortedCategories.length + 1)
+          secondCol: sortedCategories.slice(i + 1, sortedCategories.length + 1),
         };
       }
     }
@@ -348,9 +347,9 @@ export class BigBoardCore extends Component {
     const result2 = results.result2;
 
     let winningResult;
-    if (result1["npr_winner"] || results.uncontested) {
+    if (result1['npr_winner'] || results.uncontested) {
       winningResult = result1;
-    } else if (result2["npr_winner"]) {
+    } else if (result2['npr_winner']) {
       winningResult = result2;
     }
 
@@ -360,16 +359,16 @@ export class BigBoardCore extends Component {
 
     if (
       winningResult &&
-      result1["meta"]["current_party"] &&
-      winningResult["party"] !== result1["meta"]["current_party"]
+      result1['meta']['current_party'] &&
+      winningResult['party'] !== result1['meta']['current_party']
     ) {
       var change = true;
     }
 
     if (
       called ||
-      result1["votecount"] > 0 ||
-      (!results.uncontested && result2["votecount"] > 0)
+      result1['votecount'] > 0 ||
+      (!results.uncontested && result2['votecount'] > 0)
     ) {
       var reporting = true;
     }
@@ -380,30 +379,30 @@ export class BigBoardCore extends Component {
   sortBuckets(buckets) {
     return isBucketedByTime(buckets[0].category)
       ? buckets.sort(function (a, b) {
-          var aHour = parseInt(a.category.split(":")[0]);
-          var bHour = parseInt(b.category.split(":")[0]);
+          var aHour = parseInt(a.category.split(':')[0]);
+          var bHour = parseInt(b.category.split(':')[0]);
 
-          if (a.category.slice(-4) === "a.m.") return 1;
-          if (b.category.slice(-4) === "a.m.") return -1;
-          if (aHour === bHour && a.category.indexOf("30") !== -1) return 1;
-          if (aHour === bHour && b.category.indexOf("30") !== -1) return -1;
+          if (a.category.slice(-4) === 'a.m.') return 1;
+          if (b.category.slice(-4) === 'a.m.') return -1;
+          if (aHour === bHour && a.category.indexOf('30') !== -1) return 1;
+          if (aHour === bHour && b.category.indexOf('30') !== -1) return -1;
           else return aHour - bHour;
         })
       : buckets.sort((a, b) => (a.category > b.category ? 1 : -1));
   }
 
   decideLabel(race) {
-    if (race["officename"] === "U.S. House") {
-      return race["statepostal"] + "-" + race["seatnum"];
+    if (race['officename'] === 'U.S. House') {
+      return race['statepostal'] + '-' + race['seatnum'];
     } else if (
-      race["officename"] === "President" &&
-      race["level"] === "district" &&
-      race["reportingunitname"] !== "At Large"
+      race['officename'] === 'President' &&
+      race['level'] === 'district' &&
+      race['reportingunitname'] !== 'At Large'
     ) {
-      return race["statepostal"] + "-" + race["reportingunitname"].slice("-1");
-    } else if (race["is_ballot_measure"]) {
+      return race['statepostal'] + '-' + race['reportingunitname'].slice('-1');
+    } else if (race['is_ballot_measure']) {
       // The AP provides ballot measure names in inconsistent formats
-      const splitName = race.seatname.split(" - ");
+      const splitName = race.seatname.split(' - ');
       const isHyphenatedMeasureName = Boolean(
         race.seatname.match(/^[A-Z\d]+-[A-Z\d]+ /)
       );
@@ -413,8 +412,8 @@ export class BigBoardCore extends Component {
         return `${race.statepostal}: ${race.seatname}`;
       } else if (splitName.length === 1 && isHyphenatedMeasureName) {
         // Sometimes there's a compound identifier, such as '18-1 Legalize Marijuana'
-        const [number, ...identifierParts] = race.seatname.split(" ");
-        const identifier = identifierParts.join(" ");
+        const [number, ...identifierParts] = race.seatname.split(' ');
+        const identifier = identifierParts.join(' ');
         return `${race.statepostal}-${number}: ${identifier}`;
       } else if (splitName.length === 2) {
         // Usually, there's an identifier with a ` - ` delimiter, eg:
@@ -422,11 +421,11 @@ export class BigBoardCore extends Component {
         // '1464 - Campaign Finance'
         return `${race.statepostal}-${splitName[0]}: ${splitName[1]}`;
       } else {
-        console.error("Cannot properly parse the ballot measure name");
+        console.error('Cannot properly parse the ballot measure name');
         return `${race.statepostal} - ${race.seatname}`;
       }
     } else {
-      return race["statepostal"];
+      return race['statepostal'];
     }
   }
 
@@ -442,12 +441,12 @@ export class BigBoardCore extends Component {
     for (var i = 0; i < race.length; i++) {
       var result = race[i];
       if (
-        (result["party"] === "Dem" || result["party"] === "Yes") &&
+        (result['party'] === 'Dem' || result['party'] === 'Yes') &&
         !result1
       ) {
         result1 = result;
       } else if (
-        (result["party"] === "GOP" || result["party"] === "No") &&
+        (result['party'] === 'GOP' || result['party'] === 'No') &&
         !result2
       ) {
         result2 = result;
@@ -461,9 +460,9 @@ export class BigBoardCore extends Component {
     // Handle when there're two candidates of one party, and
     // ensure that the same candidate isn't used twice
     if (!result1) {
-      result1 = race.filter((r) => !areCandidatesSame(r, result2))[0];
+      result1 = race.filter(r => !areCandidatesSame(r, result2))[0];
     } else if (!result2) {
-      result2 = race.filter((r) => !areCandidatesSame(r, result1))[0];
+      result2 = race.filter(r => !areCandidatesSame(r, result1))[0];
     }
 
     let sortedResults = [result1, result2];
@@ -472,9 +471,9 @@ export class BigBoardCore extends Component {
     // Otherwise, put the leader on the left side.
     if (result1.party === result2.party) {
       sortedResults = sortedResults.sort(function (a, b) {
-        return sortedResults[0].party === "GOP"
-          ? a["votepct"] - b["votepct"]
-          : b["votepct"] - a["votepct"];
+        return sortedResults[0].party === 'GOP'
+          ? a['votepct'] - b['votepct']
+          : b['votepct'] - a['votepct'];
       });
     }
 
@@ -483,9 +482,9 @@ export class BigBoardCore extends Component {
 
   calculatePrecinctsReporting(pct) {
     if (pct > 0 && pct < 0.005) {
-      return "<1";
+      return '<1';
     } else if (pct > 0.995 && pct < 1) {
-      return ">99";
+      return '>99';
     } else {
       return Math.round(pct * 100);
     }
@@ -493,14 +492,14 @@ export class BigBoardCore extends Component {
 }
 
 const createClassesForBoardCells = (result, coloredParties, uncontested) => {
-  let classes = result.npr_winner ? "winner " : "";
+  let classes = result.npr_winner ? 'winner ' : '';
   classes += result.party.toLowerCase();
   classes +=
-    !coloredParties.includes(result.party) && result.party !== "Uncontested"
-      ? " other"
-      : "";
-  classes += result.incumbent ? " incumbent" : "";
-  classes += result.last.length > 8 ? " longname" : "";
+    !coloredParties.includes(result.party) && result.party !== 'Uncontested'
+      ? ' other'
+      : '';
+  classes += result.incumbent ? ' incumbent' : '';
+  classes += result.last.length > 8 ? ' longname' : '';
   return classes;
 };
 
@@ -509,5 +508,5 @@ const areCandidatesSame = (c1, c2) =>
   c1.first === c2.first && c1.last === c2.last && c1.party === c2.party;
 
 // TODO: make this a regex
-const isBucketedByTime = (bucket) =>
-  bucket.includes(":") && (bucket.includes("a.m.") || bucket.includes("p.m."));
+const isBucketedByTime = bucket =>
+  bucket.includes(':') && (bucket.includes('a.m.') || bucket.includes('p.m.'));
