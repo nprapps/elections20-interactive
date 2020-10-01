467488d2d9cd24397a56dff66cba2f8b7e03ed1a
ruthwtalbot <59851648+ruthwtalbot@users.noreply.github.com>
2020-10-01 10:24:43 -0700

    Make get_census_data.py handle subject tables and do calculations

--- get_census_data.py
+++ get_census_data.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import censusdata
+import requests
 
-tables = {'B01003_001E' : 'population', "B02001_001E": "race_total", "B02001_003E": "black_total"}
+detail_tables = {'B01003_001E' : 'population', "B02001_001E": "race_total", "B02001_003E": "black_total", "B03002_001E": "race_hispanic_total", "B03002_003E": "white_alone", "B03002_012E" : "hispanic_total"}
+subject_tables = {"S1501_C02_015E": "percent_bachelors", "S1901_C01_012E": "median_income"}
+
 columns = ['fips']
-columns.extend(list(tables.values()))
-# S1501_C02_015E for bachelors or higher percent?
+columns.extend(list(detail_tables.values()) + list(subject_tables.values()))
 
 def main():
   data = getAllCounties()
   processed_data = processData(data)
+
+  processed_data.to_csv('data/census_data.csv', index=False)
 
 def getAllCounties():
   states = censusdata.geographies(censusdata.censusgeo([('state', '*')]), 'acs5', 2018)
@@ -24,24 +28,31 @@ def getAllCounties():
       # Have to do it by county to preserve the fips code.
       # TODO: check if it's possible to avoid this.
       county_fips = counties[county].geo[1][1]
+      print('getting data for: ', state_fips, county_fips)
 
-      data = censusdata.download('acs5', 2018,
+      detail_data = censusdata.download('acs5', 2018,
              censusdata.censusgeo([('state', state_fips),
                                    ('county', county_fips)]),
-            list(tables.keys()))
+            list(detail_tables.keys())).reset_index()
 
-      data = data.reset_index()
+      subject_data = censusdata.download('acs5', 2018,
+             censusdata.censusgeo([('state', state_fips),
+                                   ('county', county_fips)]),
+            list(subject_tables.keys()), tabletype='subject').reset_index()
+
+      data = detail_data.merge(subject_data)
       data['index'] = state_fips + county_fips
 
       all_states = pd.concat([all_states, data])
-      # Temporary break so that we don't yet do this for all states
-      break
 
   all_states.set_axis(columns, axis=1, inplace=True)
   return all_states
 
 def processData(data):
   # TODO: process the data here
+  data['percent_black'] = data['black_total']/data['race_total']
+  data['percent_white'] = data['white_alone'] / data['race_hispanic_total']
+  data['percent_hispanic'] = data['hispanic_total'] / data['race_hispanic_total']
   return data
 
 if __name__ == "__main__":
