fb7f6ee591bcdf0cd6f9d443d4018f358a77f4ec
Audrey Carlsen <audreycarlsen@gmail.com>
2020-10-06 15:51:11 -0400

    Add eevp and incumbent to resultsBoardNamed

--- src/js/components/resultsBoardNamed/index.js
+++ src/js/components/resultsBoardNamed/index.js
@@ -5,14 +5,21 @@ import "./resultsBoardNamed.less";
 export default function(props) {
   console.log("props",props)
 
+  var sortValue = function(p) {
+    return p == "GOP" ? Infinity : p == "Dem" ? -Infinity : 0;
+  } 
+
   return (
     <table class="named results table">
       {props.races.map(r => (
         <tr>
           <td class="state">{r.state}</td>
-          <td class="reporting">TK% in</td>
-          {r.candidates.slice(0,2).map(c => (
-            <td class={"candidate " + c.party}>{c.last} <span class="perc">{c.percent}%</span></td>
+          <td class="reporting">{r.eevp}% in</td>
+          {r.candidates.slice(0,2).sort((a,b) => (sortValue(a.party) - sortValue(b.party))).map(c => (
+            <td class={"candidate " + c.party}>
+              {c.last} {c.incumbent ? <span>●</span> : ""}
+              <span class="perc">{Math.round(c.percent*100)}%</span>
+            </td>
           ))}
         </tr>
       ))}
