a583d1e754c55519d0513c2b62f17161050697b1
thomaswilburn <twilburn@npr.org>
2020-10-07 12:49:48 -0500

    Add timed suppression for about box
    Currently set at three hours hidden if you close it.

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,17 @@ if (appContainer) {
   render(<App />, appContainer);
 }
 
+// lifetime for about box supression
+var SUPPRESS = 3 * 60 * 60; // three hours
+var COOKIE = "suppress-about-box";
+
+var hideBox = function() {
+  $.one(".about-box").classList.add("closed");
+  document.cookie = `${COOKIE}=true;max-age=${SUPPRESS}`;
+}
+
 // hide about box on click
-$.one(".about-box .close").addEventListener("click", function() {
-	$.one(".about-box").classList.add("closed");
-});
+$.one(".about-box .close").addEventListener("click", hideBox);
+
+// check the cookie and hide if found
+if (document.cookie.indexOf(COOKIE) > -1) hideBox();
