253e12b7078899e13363f3bd8364e7ea33caf546
Audrey Carlsen <audreycarlsen@gmail.com>
2020-10-07 17:56:23 -0400

    Runoff tag for resultsBoardNamed

--- src/js/components/resultsBoardNamed/index.js
+++ src/js/components/resultsBoardNamed/index.js
@@ -14,14 +14,15 @@ export default function(props) {
       {props.races.map(r => (
         <tr>
           <td class="state">{r.state}{r.seatNumber ? "-" + r.seatNumber : ""}</td>
-          <td class="reporting">{r.eevp}% in</td>
+          <td class="reporting">{r.eevp ? (r.eevp + "% in"): (r.reporting && r.precincts) ? (((r.reporting / r.precincts)*100) + "% in") : ""}</td>
           {r.candidates[0].leading = true}
           {r.candidates.slice(0,2).sort((a,b) => (sortValue(a.party) - sortValue(b.party))).map(c => (
-            <td class={"candidate " + c.party + (r.eevp > 50 & c.leading ? " leading" : "") + (c.winner == "X" ? " winner" : "")}>
+            <td class={"candidate " + c.party + (r.eevp > 50 & c.leading ? " leading" : "") + (c.winner == "X" ? " winner" : "") + (r.runoff ? " runoff" : "")}>
               {c.last} {c.incumbent ? <span>●</span> : ""}
               <span class="perc">{Math.round(c.percent*100)}%</span>
             </td>
           ))}
+          <td class="runoff-label">{r.runoff ? "RUNOFF" : ""}</td>
         </tr>
       ))}
     </table>
