b7985c3f39cabcd97d43edf5359c8a0c8b6cb574
thomaswilburn <twilburn@npr.org>
2020-10-16 16:07:17 -0500

    Functioning individual embed selection

--- src/js/customizer.js
+++ src/js/customizer.js
@@ -1,6 +1,7 @@
 import { h, Fragment, render, Component } from "preact";
 import $ from "./lib/qsa";
 import stateSheet from "states.sheet.json";
+import strings from "strings.sheet.json";
 import Sidechain from "@nprapps/sidechain";
 
 class Customizer extends Component {
@@ -9,19 +10,39 @@ class Customizer extends Component {
     this.state = {
       mode: "page",
       selectedState: "AK",
-      selectedOffice: ""
+      selectedOffice: "",
+      races: [],
+      raceID: null
     }
 
     this.selectStatePage = this.selectStatePage.bind(this);
     this.selectStateOffice = this.selectStateOffice.bind(this);
+    this.loadStateRaces = this.loadStateRaces.bind(this);
+    this.selectRace = this.selectRace.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadStateRaces(this.state.selectedState);
   }
 
   selectStatePage(e) {
-    this.setState({ selectedState: e.target.value });
+    this.setState({ selectedState: e.target.value, selectedOffice: null });
+    this.loadStateRaces(e.target.value);
   }
 
   selectStateOffice(e) {
     this.setState({ selectedOffice: e.target.value })
+  }
+
+  selectRace(e) {
+    this.setState({ raceID: e.target.value })
+  }
+
+  async loadStateRaces(state) {
+    this.setState({ races: [], raceID: null })
+    var response = await fetch(`./data/states/${state}.json`);
+    var json = await response.json();
+    this.setState({ races: json.results });
   }
 
   render(props, state) {
@@ -47,7 +68,7 @@ class Customizer extends Component {
         <input
           type="radio"
           name="mode"
-          onInput={() => this.setState({ mode: data })}
+          onInput={() => this.setState({ mode: data, selectedOffice: null })}
           id={`mode-${data}`}
           checked={data == state.mode}
         />
@@ -75,7 +96,32 @@ class Customizer extends Component {
             </>);
 
           case "race":
-            return "Individual race";
+            var src = "";
+            if (state.raceID) {
+              var assembly = new URL(url + "./embed.html");
+              assembly.searchParams.set("file", `states/${state.selectedState}`);
+              assembly.searchParams.set("race", state.raceID);
+              src = assembly.toString();
+            }
+            return (<>
+              <div class="state-select">
+                <select value={state.selectedState} onInput={e => this.loadStateRaces(e.target.value)}>
+                  {postals.map(s => <option value={s}>{stateSheet[s].name}</option>)}
+                </select>
+                <select value={state.selectedRace} onInput={this.selectRace}>
+                  <option value="">Select an office</option>
+                  {state.races.map(r => <option value={r.id}>
+                    {`${strings["office-" + r.office]} ${r.seat ? r.seat : ""}`}
+                  </option>)}
+                </select>
+              </div>
+              {state.raceID && 
+                <side-chain
+                  key={state.raceID}
+                  src={src}
+                />
+              }
+            </>);
         }
       })()}
     </>)
