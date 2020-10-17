52626f71a2757d8c9888b55dc61bae3639a88c08
thomaswilburn <twilburn@npr.org>
2020-10-17 12:11:36 -0500

    Outlined tetris

--- src/js/components/tetris.js
+++ src/js/components/tetris.js
@@ -23,52 +23,105 @@ export default function Tetris(props) {
 
   var { width } = props;
   var cellSize = 20;
+  var textSize = cellSize * .7;
+  var victoryRow = 270 / width;
+
+  var last = a => a[a.length - 1];
+  var middle = a => a[a.length >> 1];
+  var avg = a => a.reduce((t, v) => t + v, 0) / a.length;
+
+  var grid = [];
+  var poke = function(r, c, v) {
+    if (!grid[r]) grid[r] = [];
+    grid[r][c] = v;
+  }
+
+  var peek = function(r, c) {
+    if (!grid[r]) return null;
+    return grid[r][c] || null;
+  }
 
   var { races } = props;
   var snake = Snake(width);
   var shapes = races.reverse().map(function(r) {
     var cells = [];
     for (var i = 0; i < r.electoral; i++) {
-      cells.push(snake.next().value);
+      var c = snake.next().value;
+      cells.push(c);
+      poke(c.row, c.column, r.state);
     }
     return {
+      data: r,
       label: r.state,
       cells
     }
   });
 
-  var last = a => a[a.length - 1];
-  var middle = a => a[a.length >> 1];
   var topShape = last(shapes);
-  var rows = Math.max(30, last(topShape.cells).row);
+  var rows = Math.max(33, victoryRow, last(topShape.cells).row) + 1;
 
-  var makePath = function(cells) {
-    return cells.map((c, i) => (i ? "L" : "M") + [
-      c.column * cellSize + cellSize / 2,
-      (rows - c.row) * cellSize + cellSize / 2
-    ].join()).join(" ");
+  var lines = [];
+  for (var i = 1; i < rows; i++) {
+    lines.push(i);
   }
 
   return <svg class={"tetris " + props.class} width={width * cellSize} height={rows * cellSize}>
+    {lines.map(g => (
+      <line 
+        x1={0} x2={width * cellSize}
+        y1={(rows - g) * cellSize} y2={(rows - g) * cellSize}
+        class="grid"
+      />
+    ))}
+    <line class="victory grid"
+      x1={0} x2={width * cellSize}
+      y1={(rows - victoryRow) * cellSize} y2={(rows - victoryRow) * cellSize}
+    />
+    <text x={0} y={(rows - victoryRow) * cellSize + textSize}>270 votes - victory</text>
     {shapes.map(function(shape, i) {
-      var center = middle(shape.cells);
+      var [ _, labelCell ] = shape.cells;
       return <>
         <g data-state={shape.label} data-count={shape.cells.length}>
-        {shape.cells.map(c => <rect
-          width={cellSize}
-          height={cellSize}
-          x={c.column * cellSize}
-          y={(rows - c.row - 1) * cellSize}
-          fill={i % 2 ? "#888" : "#555"}
-          class={i % 2 ? "a" : "b"}
-        />)}
+        {shape.cells.map(function(c) {
+          var attrs = {
+            x: c.column * cellSize,
+            y: (rows - c.row - 1) * cellSize,
+            width: cellSize,
+            height: cellSize
+          };
+          // get neighboring cells
+          var neighbors = {
+            top: peek(c.row + 1, c.column),
+            bottom: peek(c.row - 1, c.column),
+            left: peek(c.row, c.column - 1),
+            right: peek(c.row, c.column + 1)
+          };
+          // do those neighbors match?
+          for (var k in neighbors) neighbors[k] = neighbors[k] == shape.label;
+          // shrink the rectangle if it's lonely
+          if (!neighbors.top) {
+            attrs.y++;
+            attrs.height--;
+          }
+          if (!neighbors.bottom) {
+            attrs.height--;
+          }
+          if (!neighbors.left) {
+            attrs.x++
+            attrs.width--;
+          }
+          if (!neighbors.right) {
+            attrs.width--;
+          }
+          return <rect {...attrs} class={ i % 2 ? "a" : "b"} />
+        })}
         </g>
         <text 
-          x={center.column * cellSize + cellSize / 2} 
-          y={(rows - center.row) * cellSize - 2}
+          x={labelCell.column * cellSize + cellSize / 2} 
+          y={(rows - labelCell.row) * cellSize - (cellSize - textSize) / 2}
           text-anchor="middle"
           fill="white"
-          font-size={cellSize - 3}
+          font-size={textSize}
         >
           {shape.label}
         </text>
