363be901f2b2f1db379ab02965cc4867e6e69a10
Audrey Carlsen <audreycarlsen@gmail.com>
2020-10-19 12:22:17 -0400

    Show 0% in if race is called for president

--- src/js/components/resultsBoardPresident/index.js
+++ src/js/components/resultsBoardPresident/index.js
@@ -48,7 +48,7 @@ export default function ResultsBoardPresident(props) {
             {props.races.map(function(r, i) {
               var hasResult = r.eevp || r.reporting || r.called || r.runoff;
               var reporting = r.eevp || r.reportingPercent;
-              var percentIn = reporting ? reportingPercentage(reporting) + "% in" : "";
+              var percentIn = reporting || reporting == 0 ? reportingPercentage(reporting) + "% in" : "";
               var winner = r.candidates.filter(c => c.winner == "X");
               var flipped = winner[0] && (r.previousParty !== winner[0].party);
               var stateDetail = states[r.state] || {};
