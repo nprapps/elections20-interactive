59514cf3f401ec052d67661c91b460e35804eb1c
Audrey Carlsen <audreycarlsen@gmail.com>
2020-10-22 11:02:37 -0400

    BOP embed updates

--- src/js/embedBOP.js
+++ src/js/embedBOP.js
@@ -10,7 +10,7 @@ var guest = Sidechain.registerGuest();
 gopher.watch("./data/bop.json", init);
 
 var winnerIcon =
-  `<span class="winner-icon" role="img" aria-label="check mark">
+  <span class="winner-icon" role="img" aria-label="check mark">
     <svg
       aria-hidden="true"
       focusable="false"
@@ -21,7 +21,7 @@ var winnerIcon =
         fill="#333"
         d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path>
     </svg>
-  </span>`;
+  </span>;
 
 async function init(results) {
   var president = {
@@ -56,21 +56,25 @@ async function init(results) {
     <div class="chatter"><strong>270</strong> electoral votes needed to win</div>
     <div class="pres-container">
       <div class="candidate">
-        <div class="name">Biden {president.winner == "Biden" ? "✔" : ""}</div>
+        <div class="name">Biden {president.winner == "Biden" ? winnerIcon : ""}</div>
         <div class="votes dem">{president.Dem}</div>
       </div>
       <div class="candidate">
-        <div class="name">Trump {president.winner == "Trump" ? "✔" : ""}</div>
+        <div class="name">Trump {president.winner == "Trump" ? winnerIcon : ""}</div>
         <div class="votes gop">{president.GOP}</div>
       </div>
-      <div class="candidate">
-        <div class="name">Other {president.winner == "Other" ? "✔" : ""}</div>
-        <div class="votes other">{president.Other}</div>
-      </div>
-      <div class="candidate">
-        <div class="name">Uncalled</div>
-        <div class="votes">{538 - president.Dem - president.GOP - president.Other}</div>
-      </div>
+      {president.Other ?
+        <div class="candidate">
+          <div class="name">Other {president.winner == "Other" ? winnerIcon : ""}</div>
+          <div class="votes other">{president.Other}</div>
+        </div>
+      : ""}
+      {538 - president.Dem - president.GOP - president.Other ?
+        <div class="candidate">
+          <div class="name">Uncalled</div>
+          <div class="votes">{538 - president.Dem - president.GOP - president.Other}</div>
+        </div>
+      : ""}
     </div>
 
     <div class="divider" />
@@ -79,13 +83,13 @@ async function init(results) {
     <div class="chatter"><strong>218</strong> seats needed for majority</div>
     <div class="bar-container">
       <div class="bar dem" style={"width: " + (house.Dem / 435 * 100) + "%"}>
-        <div class="label">Dem. {house.Dem >= 218 ? "✔" : ""}<span>{house.Dem}</span></div>
+        <div class="label">Dem. {house.Dem >= 218 ? winnerIcon : ""}<span class="number">{house.Dem}</span></div>
       </div>
       <div class="bar other" style={"width: " + (house.Other / 435 * 100) + "%"}>
-        <div class="label">Ind. {house.Other >= 218 ? "✔" : ""}<span>{house.Other}</span></div>
+        {house.Other ? <div class="label">Ind. {house.Other >= 218 ? winnerIcon : ""}<span class="number">{house.Other}</span></div> : ""}
       </div>
       <div class="bar gop" style={"width: " + (house.GOP / 435 * 100) + "%"}>
-        <div class="label">GOP {house.GOP >= 218 ? "✔" : ""}<span>{house.GOP}</span></div>
+        <div class="label">GOP {house.GOP >= 218 ? winnerIcon : ""}<span class="number">{house.GOP}</span></div>
       </div>
       <div class="middle"></div>
     </div>
@@ -94,13 +98,13 @@ async function init(results) {
     <div class="chatter"><strong>51</strong> seats needed for majority</div>
     <div class="bar-container">
       <div class="bar dem" style={"width: " + (senate.Dem) + "%"}>
-        <div class="label">Dem. {senate.Dem >= 51 ? "✔" : ""}<span>{senate.Dem}</span></div>
+        <div class="label">Dem. {senate.Dem >= 51 ? winnerIcon : ""}<span class="number">{senate.Dem}</span></div>
       </div>
       <div class="bar other" style={"width: " + (senate.Other) + "%"}>
-        <div class="label">Ind. {senate.Other >= 51 ? "✔" : ""}<span>{senate.Other}</span></div>
+        <div class="label">Ind. {senate.Other >= 51 ? winnerIcon : ""}<span class="number">{senate.Other}</span></div>
       </div>
       <div class="bar gop" style={"width: " + (senate.GOP) + "%"}>
-        <div class="label">GOP {senate.GOP >= 51 ? "✔" : ""}<span>{senate.GOP}</span></div>
+        <div class="label">GOP {senate.GOP >= 51 ? winnerIcon : ""}<span class="number">{senate.GOP}</span></div>
       </div>
       <div class="middle"></div>
     </div>
