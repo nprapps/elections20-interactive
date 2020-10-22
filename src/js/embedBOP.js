41b54b84be133de9231c3fceedcbddd7edc69b83
Audrey Carlsen <audreycarlsen@gmail.com>
2020-10-21 21:05:02 -0400

    BOP embed labels and styling

--- src/js/embedBOP.js
+++ src/js/embedBOP.js
@@ -36,28 +36,60 @@ async function init(results) {
 
   var template = <>
 
-    President:
-    <div>{president.Dem} Biden</div>
-    <div>{president.GOP} Trump</div>
-    {president.Other ? <div>{president.Other} Other</div> : ""}
+    <h3>President</h3>
+    <div class="chatter"><strong>270</strong> electoral votes needed to win</div>
+    <div class="pres-container">
+      <div class="candidate">
+        <div class="name">Biden</div>
+        <div class="votes dem">{president.Dem}</div>
+      </div>
+      <div class="candidate">
+        <div class="name">Trump</div>
+        <div class="votes gop">{president.GOP}</div>
+      </div>
+      <div class="candidate">
+        <div class="name">Other</div>
+        <div class="votes other">{president.Other}</div>
+      </div>
+      <div class="candidate">
+        <div class="name">Uncalled</div>
+        <div class="votes">{538 - president.Dem - president.GOP - president.Other}</div>
+      </div>
+    </div>
 
-    House:
+    <div class="divider" />
+
+    <h3>House</h3>
+    <div class="chatter"><strong>51</strong> seats needed for majority</div>
     <div class="bar-container">
-      <div class="bar dem" style={"width: " + (house.Dem / 435 * 100) + "%"}></div>
-      <div class="bar other" style={"width: " + (house.Other / 435 * 100) + "%"}></div>
-      <div class="bar gop" style={"width: " + (house.GOP / 435 * 100) + "%"}></div>
+      <div class="bar dem" style={"width: " + (house.Dem / 435 * 100) + "%"}>
+        <div class="label">Dem. <span>{house.Dem}</span></div>
+      </div>
+      <div class="bar other" style={"width: " + (house.Other / 435 * 100) + "%"}>
+        <div class="label">Ind. <span>{house.Other}</span></div>
+      </div>
+      <div class="bar gop" style={"width: " + (house.GOP / 435 * 100) + "%"}>
+        <div class="label">GOP <span>{house.GOP}</span></div>
+      </div>
       <div class="middle"></div>
     </div>
 
-    Senate:
+    <h3>Senate</h3>
+    <div class="chatter"><strong>218</strong> seats needed for majority</div>
     <div class="bar-container">
-      <div class="bar dem" style={"width: " + (senate.Dem) + "%"}></div>
-      <div class="bar other" style={"width: " + (senate.Other) + "%"}></div>
-      <div class="bar gop" style={"width: " + (senate.GOP) + "%"}></div>
+      <div class="bar dem" style={"width: " + (senate.Dem) + "%"}>
+        <div class="label">Dem. <span>{senate.Dem}</span></div>
+      </div>
+      <div class="bar other" style={"width: " + (senate.Other) + "%"}>
+        <div class="label">Ind. <span>{senate.Other}</span></div>
+      </div>
+      <div class="bar gop" style={"width: " + (senate.GOP) + "%"}>
+        <div class="label">GOP <span>{senate.GOP}</span></div>
+      </div>
       <div class="middle"></div>
     </div>
 
-    Source: AP (as of <DateFormatter value="TK" />)
+    <div class="source">Source: AP (as of <DateFormatter value={results.latest} />)</div>
 
   </>;
   
