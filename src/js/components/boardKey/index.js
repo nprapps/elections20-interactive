276085ce954010b91cd0a9b564280ab31654034c
Audrey Carlsen <audreycarlsen@gmail.com>
2020-10-23 09:29:52 -0400

    More key edits

--- src/js/components/boardKey/index.js
+++ src/js/components/boardKey/index.js
@@ -3,7 +3,7 @@ import { h, Fragment } from "preact";
 export default function BoardKey(props) {
 	var race = props.race;
 	var hasParties = race !== "ballot";
-	var hasYesNo = race == "ballot";
+  var hasPickup = race == "house" || race == "senate";
 	var hasIncumbent = race == "house" || race == "senate" || race == "gov";
 	var hasEEVP = race !== "house";
 
@@ -14,13 +14,13 @@ export default function BoardKey(props) {
 	      	<li class="gop">Republican / <span class="leading">Leading</span> <span class="winner">Winner</span></li>
 	      	<li class="ind">Independent / <span class="leading">Leading</span> <span class="winner">Winner</span></li>
       </>}
-      {hasParties && <li class="pickup"><span>FLIP</span> TKTK Seat pickup (party color)</li>}
+      {hasParties && <li class="pickup"><span>FLIP</span> {hasPickup ? "Seat pickup" : "Changed parties since last election"} (party color)</li>}
       {hasParties && <li class="runoff"><span>R.O.</span> Going to a runoff election</li>}
-      {hasYesNo && <>
+      {!hasParties && <>
       	<li class="yes">Yes / <span class="leading">Leading</span> <span class="winner">Winner</span></li>
       	<li class="no">No / <span class="leading">Leading</span> <span class="winner">Winner</span></li>
       </>}
-      <li class="eevp"><span>76% in</span> {hasEEVP ? "TKTK EEVP" : "TKTK Precincts reporting"}</li>
+      <li class="eevp"><span>76% in</span> {hasEEVP ? "Estimated vote" : "Precincts reporting"}</li>
       {hasIncumbent && <li class="incumbent">● Incumbent</li>}
     </ul>
   </div>
