7782bfea91f9d6e20005fcff1740de9cbd744632
Audrey Carlsen <audreycarlsen@gmail.com>
2020-10-22 21:45:58 -0400

    EEVP language in key

--- src/js/components/boardKey/index.js
+++ src/js/components/boardKey/index.js
@@ -5,6 +5,7 @@ export default function BoardKey(props) {
 	var hasParties = race !== "ballot";
 	var hasYesNo = race == "ballot";
 	var hasIncumbent = race == "house" || race == "senate" || race == "gov";
+	var hasEEVP = race !== "house";
 
   return <div class="board-key">
     <ul>
@@ -13,14 +14,14 @@ export default function BoardKey(props) {
 	      	<li class="gop">Republican / <span class="leaning">Leaning</span> <span class="winner">Winner</span></li>
 	      	<li class="ind">Independent / <span class="leaning">Leaning</span> <span class="winner">Winner</span></li>
       </>}
-      {hasIncumbent && <li class="incumbent">● Incumbent</li>}
       {hasParties && <li class="pickup"><span>FLIP</span> TKTK Seat pickup (party color)</li>}
       {hasParties && <li class="runoff"><span>R.O.</span> Going to a runoff election</li>}
       {hasYesNo && <>
       	<li class="yes">Yes / <span class="leaning">Leaning</span> <span class="winner">Winner</span></li>
       	<li class="no">No / <span class="leaning">Leaning</span> <span class="winner">Winner</span></li>
       </>}
-      <li class="eevp"><span>76% in</span> TKTK Precincts reporting</li>
+      <li class="eevp"><span>76% in</span> {hasEEVP ? "TKTK EEVP" : "TKTK Precincts reporting"}</li>
+      {hasIncumbent && <li class="incumbent">● Incumbent</li>}
     </ul>
   </div>
 }
