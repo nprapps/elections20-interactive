eb7b93eceb1565f152987f2c50b33d1c254785cc
Audrey Carlsen <audreycarlsen@gmail.com>
2020-10-23 09:06:37 -0400

    Leading, not leaning

--- src/js/components/boardKey/index.js
+++ src/js/components/boardKey/index.js
@@ -10,15 +10,15 @@ export default function BoardKey(props) {
   return <div class="board-key">
     <ul>
       {hasParties && <>
-	      	<li class="dem">Democrat / <span class="leaning">Leaning</span> <span class="winner">Winner</span></li>
-	      	<li class="gop">Republican / <span class="leaning">Leaning</span> <span class="winner">Winner</span></li>
-	      	<li class="ind">Independent / <span class="leaning">Leaning</span> <span class="winner">Winner</span></li>
+	      	<li class="dem">Democrat / <span class="leading">Leading</span> <span class="winner">Winner</span></li>
+	      	<li class="gop">Republican / <span class="leading">Leading</span> <span class="winner">Winner</span></li>
+	      	<li class="ind">Independent / <span class="leading">Leading</span> <span class="winner">Winner</span></li>
       </>}
       {hasParties && <li class="pickup"><span>FLIP</span> TKTK Seat pickup (party color)</li>}
       {hasParties && <li class="runoff"><span>R.O.</span> Going to a runoff election</li>}
       {hasYesNo && <>
-      	<li class="yes">Yes / <span class="leaning">Leaning</span> <span class="winner">Winner</span></li>
-      	<li class="no">No / <span class="leaning">Leaning</span> <span class="winner">Winner</span></li>
+      	<li class="yes">Yes / <span class="leading">Leading</span> <span class="winner">Winner</span></li>
+      	<li class="no">No / <span class="leading">Leading</span> <span class="winner">Winner</span></li>
       </>}
       <li class="eevp"><span>76% in</span> {hasEEVP ? "TKTK EEVP" : "TKTK Precincts reporting"}</li>
       {hasIncumbent && <li class="incumbent">● Incumbent</li>}
