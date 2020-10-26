52aa71afc9a98c3a29bcfb5d8fa95f6fa81dd411
thomaswilburn <twilburn@npr.org>
2020-10-26 12:39:56 -0500

    Generate state share cards

--- tasks/build.js
+++ tasks/build.js
@@ -62,6 +62,15 @@ module.exports = function(grunt) {
       var output = process(input, data, src);
       grunt.file.write(file.dest, output);
     });
+
+    // generate state share pages
+    var stateTemplate = grunt.file.read("src/_state_social.html");
+    for (var state in grunt.data.json.states) {
+      var meta = grunt.data.json.states[state];
+      if (meta.district) continue;
+      var output = process(stateTemplate, { state });
+      grunt.file.write(`build/share/${state}.html`, output);
+    }
   });
 
 }
