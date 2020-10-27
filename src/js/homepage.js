226897b86eaa904d8e943d20f865b54d87b5a089
Audrey Carlsen <audreycarlsen@gmail.com>
2020-10-26 20:39:24 -0400

    Add tabs to homepage widget

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -2,8 +2,10 @@ import { h, Fragment, Component, render } from "preact";
 import { ElectoralBars, Leaderboard } from "./components/boardPresident";
 import NationalMap from "./components/nationalMap";
 import ElectoralBubbles from "./components/electoralBubbles";
+import Cartogram from "./components/cartogram";
 import DateFormatter from "./components/dateFormatter";
 import TestBanner from "./components/testBanner";
+import Tabs from "./components/tabs";
 import { groupCalled, sumElectoral, getBucket } from "./components/util.js";
 import $ from "./lib/qsa";
 import gopher from "./components/gopher";
@@ -44,13 +46,30 @@ export default class Homepage extends Component {
     var called = groupCalled(results);
 
     var search = new URLSearchParams(window.location.search);
-    var Display = search.get("display") == "map" ? NationalMap : ElectoralBubbles;
+    var Display = search.get("display") == "map" ? NationalMap :
+                  search.get("display") == "cartogram" ? Cartogram : 
+                  ElectoralBubbles;
 
     return <div class="president board">
       { test ? <TestBanner /> : "" }
       <ElectoralBars called={called} />
       <Leaderboard called={called} />
-      <Display results={results} />
+
+      <Tabs id="president-viz">
+
+        <div icon="./assets/icons/ico-bubbles.svg" label="Margins">
+          <ElectoralBubbles results={results} />
+        </div>
+
+        <div icon="./assets/icons/ico-cartogram.svg" label="Electoral">
+          <Cartogram races={results} />
+        </div>
+
+        <div icon="./assets/icons/ico-geo.svg" label="Geography">
+          <NationalMap races={results} />
+        </div>
+        
+      </Tabs>
 
       { false && <div label="Board" class="board-container President">
         {results && <>
