aaeefa912363b49a9dba408136e9d310ff9dd3d4
Audrey Carlsen <audreycarlsen@gmail.com>
2020-10-27 14:57:46 -0400

    Liveblog embed: dark theme

--- src/js/embedBOP.js
+++ src/js/embedBOP.js
@@ -13,7 +13,7 @@ var search = new URLSearchParams(window.location.search);
 var params = {
   president: null,
   inline: null,
-  them: null
+  theme: null
 };
 
 for (var k in params) params[k] = search.get(k);
