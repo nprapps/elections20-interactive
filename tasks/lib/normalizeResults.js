24442b92484e5b7e68a4d4f9651ec09beb1c2db4
thomaswilburn <twilburn@npr.org>
2020-10-27 09:25:52 -0500

    Fix #89

--- tasks/lib/normalizeResults.js
+++ tasks/lib/normalizeResults.js
@@ -171,11 +171,12 @@ module.exports = function (resultArray, overrides = {}) {
         }
 
         // add the state name to states
-        var stateMeta = states[unitMeta.state];
+        var stateKey = unitMeta.district ? `${unitMeta.state}-${unitMeta.district}` : unitMeta.state;
+        var stateMeta = states[stateKey];
         if (stateMeta) {
-          unitMeta.stateName = states[unitMeta.state].name;
-          unitMeta.stateAP = states[unitMeta.state].ap;
-          unitMeta.rating = states[unitMeta.state].rating;
+          unitMeta.stateName = stateMeta.name;
+          unitMeta.stateAP = stateMeta.ap;
+          unitMeta.rating = stateMeta.rating;
         }
 
         var sheetMetadata = nprMetadata[raceMeta.id];
