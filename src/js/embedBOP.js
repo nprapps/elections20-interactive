5f780ed5bc2231b422077c9052e2218d882e276c
Audrey Carlsen <audreycarlsen@gmail.com>
2020-10-30 21:25:39 -0400

    Fix #137

--- src/js/embedBOP.js
+++ src/js/embedBOP.js
@@ -114,16 +114,7 @@ async function init(results) {
           <a class="link-container president" href="http://apps.npr.org/elections20-interactive/#/president" target="_top">
             
             <h3>President</h3>
-            <div class="full-link"><a>See full results ›</a></div>
-            <div class="chatter"><strong>270</strong> electoral votes to win</div>
             
-            <div class="bar-container">
-              <div class="bar dem" style={"width: " + (president.Dem / 538 * 100) + "%"}></div>
-              <div class="bar other" style={"width: " + (president.Other / 538 * 100) + "%"}></div>
-              <div class="bar gop" style={"width: " + (president.GOP / 538 * 100) + "%"}></div>
-              <div class="middle"></div>
-            </div>
-
             <div class="number-container">
               <div class="candidate dem">
                 <div class="name">Biden {president.Dem >= 270 ? winnerIcon : ""}</div>
@@ -146,6 +137,17 @@ async function init(results) {
                 <div class="votes">{president.GOP}</div>
               </div>
             </div>
+
+            <div class="bar-container">
+              <div class="bar dem" style={"width: " + (president.Dem / 538 * 100) + "%"}></div>
+              <div class="bar other" style={"width: " + (president.Other / 538 * 100) + "%"}></div>
+              <div class="bar gop" style={"width: " + (president.GOP / 538 * 100) + "%"}></div>
+              <div class="middle"></div>
+            </div>
+
+            <div class="chatter"><strong>270</strong> electoral votes to win</div>
+
+            <div class="full-link"><a>See full results ›</a></div>
           </a>
           <div class="divider" />
         </>}
@@ -155,32 +157,10 @@ async function init(results) {
         <a class="link-container house" href="http://apps.npr.org/elections20-interactive/#/house" target="_top">
           
           <h3>House</h3>
-          <div class="full-link"><a>See full results ›</a></div>
-          <div class="chatter"><strong>218</strong> seats for majority </div>
-          
-          <div class="net-gain-container">
-            <div class="gain-label">Net gain</div>
-            <div class={"net-gain " + house.netGainParty}>{house.netGainParty != "none"
-                ? house.netGainParty + " +" + house.netGain
-                : "No change"}</div>
-          </div>
-
-          <div class="bar-container">
-            <div class="bar dem" style={"width: " + (house.Dem.total / 435 * 100) + "%"}>
-              <div class="label">Dem. {house.Dem.total >= 218 ? winnerIcon : ""}<span class="number">{house.Dem.total}</span></div>
-            </div>
-            <div class="bar other" style={"width: " + (house.Other.total / 435 * 100) + "%"}>
-              {house.Other.total ? <div class="label">Ind. {house.Other.total >= 218 ? winnerIcon : ""}<span class="number">{house.Other.total}</span></div> : ""}
-            </div>
-            <div class="bar gop" style={"width: " + (house.GOP.total / 435 * 100) + "%"}>
-              <div class="label">GOP {house.GOP.total >= 218 ? winnerIcon : ""}<span class="number">{house.GOP.total}</span></div>
-            </div>
-            <div class="middle"></div>
-          </div>
-
+  
           <div class="number-container">
             <div class="candidate dem">
-              <div class="name">Dem. {house.Dem.total >= 218 ? winnerIcon : ""}</div>
+              <div class="name">Dem. {house.Dem.total >= 8 ? winnerIcon : ""}</div>
               <div class="votes">{house.Dem.total}</div>
             </div>
             {house.Other.total ?
@@ -201,40 +181,40 @@ async function init(results) {
             </div>
           </div>
 
-          
+          <div class="bar-container">
+            <div class="bar dem" style={"width: " + (house.Dem.total / 435 * 100) + "%"}>
+              {/*<div class="label">Dem. {house.Dem.total >= 218 ? winnerIcon : ""}<span class="number">{house.Dem.total}</span></div>*/}
+            </div>
+            <div class="bar other" style={"width: " + (house.Other.total / 435 * 100) + "%"}>
+              {/*{house.Other.total ? <div class="label">Ind. {house.Other.total >= 218 ? winnerIcon : ""}<span class="number">{house.Other.total}</span></div> : ""}*/}
+            </div>
+            <div class="bar gop" style={"width: " + (house.GOP.total / 435 * 100) + "%"}>
+              {/*<div class="label">GOP {house.GOP.total >= 218 ? winnerIcon : ""}<span class="number">{house.GOP.total}</span></div>*/}
+            </div>
+            <div class="middle"></div>
+          </div>
+
+          <div class="chatter"><strong>218</strong> seats for majority </div>
+
+          <div class="full-link"><a>See full results ›</a></div>
+          <div class="net-gain-container">
+            <div class="gain-label">Net gain</div>
+            <div class={"net-gain " + house.netGainParty}>{house.netGainParty != "none"
+                ? house.netGainParty + " +" + house.netGain
+                : "No change"}</div>
+          </div>
         </a>
+        <div class="second divider" />
 
         {/*SENATE*/}
 
         <a class="link-container senate" href="http://apps.npr.org/elections20-interactive/#/senate" target="_top">
           
           <h3>Senate</h3>
-          <div class="full-link"><a>See full results ›</a></div>
-          <div class="chatter"><strong>51</strong> seats for majority</div>
-
-          <div class="net-gain-container">
-            <div class="gain-label">Net gain</div>
-            <div class={"net-gain " + senate.netGainParty}>{senate.netGainParty != "none"
-                    ? senate.netGainParty + " +" + senate.netGain
-                    : "No change"}</div>
-          </div>
           
-          <div class="bar-container">
-            <div class="bar dem" style={"width: " + (senate.Dem.total) + "%"}>
-              <div class="label">Dem. {senate.Dem.total >= 51 ? winnerIcon : ""}<span class="number">{senate.Dem.total}</span></div>
-            </div>
-            <div class="bar other" style={"width: " + (senate.Other.total) + "%"}>
-              <div class="label">Ind. {senate.Other.total >= 51 ? winnerIcon : ""}<span class="number">{senate.Other.total}</span></div>
-            </div>
-            <div class="bar gop" style={"width: " + (senate.GOP.total) + "%"}>
-              <div class="label">GOP {senate.GOP.total >= 51 ? winnerIcon : ""}<span class="number">{senate.GOP.total}</span></div>
-            </div>
-            <div class="middle"></div>
-          </div>
-
           <div class="number-container">
             <div class="candidate dem">
-              <div class="name">Dem. {senate.Dem.total >= 51 ? winnerIcon : ""}</div>
+              <div class="name">Dem. {senate.Dem.total >= 1 ? winnerIcon : ""}</div>
               <div class="votes">{senate.Dem.total}</div>
             </div>
             {senate.Other.total ?
@@ -253,6 +233,29 @@ async function init(results) {
               <div class="name">GOP {senate.GOP.total >= 51 ? winnerIcon : ""}</div>
               <div class="votes">{senate.GOP.total}</div>
             </div>
+          </div>
+
+          <div class="bar-container">
+            <div class="bar dem" style={"width: " + (senate.Dem.total) + "%"}>
+              {/*<div class="label">Dem. {senate.Dem.total >= 51 ? winnerIcon : ""}<span class="number">{senate.Dem.total}</span></div>*/}
+            </div>
+            <div class="bar other" style={"width: " + (senate.Other.total) + "%"}>
+              {/*<div class="label">Ind. {senate.Other.total >= 51 ? winnerIcon : ""}<span class="number">{senate.Other.total}</span></div>*/}
+            </div>
+            <div class="bar gop" style={"width: " + (senate.GOP.total) + "%"}>
+              {/*<div class="label">GOP {senate.GOP.total >= 51 ? winnerIcon : ""}<span class="number">{senate.GOP.total}</span></div>*/}
+            </div>
+            <div class="middle"></div>
+          </div>
+
+          <div class="chatter"><strong>51</strong> seats for majority</div>
+
+          <div class="full-link"><a>See full results ›</a></div>
+          <div class="net-gain-container">
+            <div class="gain-label">Net gain</div>
+            <div class={"net-gain " + senate.netGainParty}>{senate.netGainParty != "none"
+                    ? senate.netGainParty + " +" + senate.netGain
+                    : "No change"}</div>
           </div>
 
         </a>
