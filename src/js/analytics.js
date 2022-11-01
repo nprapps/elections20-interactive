a19338a6b0eb789084dc6fca8dada09f4f2a3ada
kokonakajima <knakajima@npr.org>
2022-11-01 14:58:10 -0400

    add event listener for data consent changed event

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,6 +1,11 @@
 var DataConsent = require('./lib/data-consent');
 
-var setupGoogle = function() {
+var googleAnalyticsAlreadyInitialized = false;
+
+var setupGoogleAnalytics = function() {
+  // Bail early if user hasn't consented
+  if (!DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS)) return;
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -39,8 +44,16 @@ var setupGoogle = function() {
     });
     // do not send pageview because we'll handle that virtually
   }
+  googleAnalyticsAlreadyInitialized = true;
 }
 
-if (DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS)) {
-  setupGoogle();
-}
+
+var oldonload = window.onload;
+window.onload = (typeof window.onload != 'function') ? setupGoogleAnalytics : function() { oldonload(); setupGoogleAnalytics(); };
+
+document.addEventListener('npr:DataConsentChanged', () => {
+  if (googleAnalyticsAlreadyInitialized) return;
+  if (DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS)) {
+    setupGoogleAnalytics();
+  }  
+});
